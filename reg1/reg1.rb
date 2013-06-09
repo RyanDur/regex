a495b30c479e894985f1ba36c1d649bdc2a21b91
Ryan Durling <ryan.p.durling@gmail.com>
2013-06-08 19:29:30 -0700

    read from input and produce output

--- reg1/reg1.rb
+++ reg1/reg1.rb
@@ -1,18 +1,5 @@
-strings = ["Mr. Ned Beatty",
-"Diana Christensen",
-"Mr. Max Schumacher",
-"Mr. Robert Duvall",
-"Mr. Frank Hackett",
-"Beatrice Straight",
-"Faye Dunaway",
-"Mr. Howard Beale",
-"Louise Schumacher           ",
-"Mr. William Holden",
-"Mr. Arthur Jensen",
-"Carolyn Krigbaum",
-"Cindy Grover",
-"Mr. Peter Finch"]
+output = File.new("output.txt","w+")
 
-strings.each{|e| puts e.gsub(/(Mr. ?)|^/, "Ms. ")}
-
-strings.each{|e| puts e.gsub(/ (\w+)(\w+)/, "")}
+File.open("input.txt","r").each do |line| 
+  output.puts line.gsub(/(Mr. ?)|^/,"Ms. ")
+end
