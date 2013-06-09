fb89e02d1ce33fd2c327a806b0574d0daf152ff2
Ryan Durling <ryan.p.durling@gmail.com>
2013-06-08 23:43:13 -0700

    improve date regex

--- reg4/reg4.rb
+++ reg4/reg4.rb
@@ -1,6 +1,9 @@
 output = File.new('output.txt', 'w+')
 
 File.open('input.txt').each_line do |line|
+  line.gsub!(/^(\d{1})-/, '0\1-')
+  line.gsub!(/-(\d{1})-/, '-0\1-')
   line.gsub!(/-(\d{2})$/, '-20\1')
+  line.gsub!(/(\d{2})-(\d{2})-(\d{4})/, '\3-\1-\2')
   output.puts line
 end
