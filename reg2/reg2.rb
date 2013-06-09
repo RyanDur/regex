46c8df03384d5e1357f7a086dd4b512c346213f1
Ryan Durling <ryan.p.durling@gmail.com>
2013-06-08 22:21:16 -0700

    remove whitspace after first number

--- reg2/reg2.rb
+++ reg2/reg2.rb
@@ -1,6 +1,6 @@
 output = File.new("output.txt", "w+")
 
 File.open("input.txt").each_line do |line|
-  line.gsub!(/^\s*\d*|\[\d+\]/,'')
+  line.gsub!(/^\s*\d*\s*|\[\d+\]/,'')
   output.puts line unless line.empty?
 end
