3a2c8a69e7825e9d5417452c2e18e0ef3160d768
Ryan Durling <ryan.p.durling@gmail.com>
2013-06-08 22:08:29 -0700

    modify regex

--- reg2/reg2.rb
+++ reg2/reg2.rb
@@ -1,6 +1,6 @@
 output = File.new("output.txt", "w+")
 
 File.open("input.txt").each_line do |line|
-  line.gsub!(/\[\d+\]|^\s+/,'')
+  line.gsub!(/^\s*\d*|\[\d+\]/,'')
   output.puts line unless line.empty?
 end
